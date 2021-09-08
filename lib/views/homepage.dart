dacca6de3df4d2e9f2bc29c6dd36565cbbb0dde3
Michael Mekuleyi <mekuleyimichael@gmail.com>
2021-09-08 04:04:20 +0100

    added all variables and refactored

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -20,26 +20,141 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  // TextControllers
+  TextEditingController lengthOfTheRoad;
+  TextEditingController widthOfTheRoad;
+  TextEditingController numberOf100Lightening;
+  TextEditingController numberOf50Lightening;
+  TextEditingController voltageOfBattery;
+  TextEditingController wattsOfEachSolarPanel;
+  TextEditingController daysOfAutonomyOfBattery;
+  TextEditingController wattsOfEachLed;
+  TextEditingController ampereHrOfEachBattery;
+
+  //keyform state
+  final _numberOfPanelsFormState = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    lengthOfTheRoad = TextEditingController();
+    widthOfTheRoad = TextEditingController();
+    numberOf100Lightening = TextEditingController();
+    numberOf50Lightening = TextEditingController();
+    voltageOfBattery = TextEditingController();
+    wattsOfEachSolarPanel = TextEditingController();
+    daysOfAutonomyOfBattery = TextEditingController();
+    wattsOfEachLed = TextEditingController();
+    ampereHrOfEachBattery = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    lengthOfTheRoad.dispose();
+    widthOfTheRoad.dispose();
+    numberOf100Lightening.dispose();
+    numberOf50Lightening.dispose();
+    voltageOfBattery.dispose();
+    wattsOfEachSolarPanel.dispose();
+    daysOfAutonomyOfBattery.dispose();
+    wattsOfEachLed.dispose();
+    ampereHrOfEachBattery.dispose();
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: [
+      body: SingleChildScrollView(
+        child: Form(
+          key: _numberOfPanelsFormState,
+          child: Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Column(
 
-        ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                VariableWidget(variableName: "Length Of Road", controller: lengthOfTheRoad,),
+                FlatButton(onPressed: () {
+                  print(lengthOfTheRoad.text);
+                }, child: Text("Submit"))
+
+              ],
+            ),
+          ),
+        ),
       )
 // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class VariableWidget extends StatefulWidget {
+  TextEditingController controller;
+  String variableName;
+
+  VariableWidget({
+    Key key, this.controller, this.variableName
+  }) : super(key: key);
+
+  @override
+  _VariableWidgetState createState() => _VariableWidgetState();
+}
+
+class _VariableWidgetState extends State<VariableWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        children: [
+          Text(
+            widget.variableName,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 14),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            autovalidateMode:
+            AutovalidateMode.onUserInteraction,
+            validator: (String value) {
+              if (value.isEmpty) {
+                return "The field cannot be left empty";
+              }
+
+              return null;
+            },
+            style: TextStyle(fontSize: 16),
+            controller: widget.controller,
+            decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)),
+                focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)),
+                contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10, vertical: 0),
+                hintText: "Name Of Business",
+                hintStyle: TextStyle(
+                    color: Color(0xff747272), fontSize: 16),
+                border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius.all(Radius.circular(10)))),
+          ),
+        ],
+      ),
     );
   }
 }
